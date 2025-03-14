b70d083389cb89082d982417e2633452dc5606df
Laranurmerve <mervelara1453@gmail.com>
2025-03-15 00:29:35 +0300

    ....

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -11,12 +11,66 @@ namespace _08_Methods
         static void Main(string[] args)
         {
 
-            #region Metotlar 
+            #region Void Metotlar 
 
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Nursena");
+            //    Console.WriteLine("Büşra");
+            //    Console.WriteLine("Merve");
+            //    Console.WriteLine("Sena");
+            //}
 
+            //CustomerList();
 
+            //void sum()
+            //{
+            //    int x = 5;
+            //    int y = 10;
+            //    int z = x + y;
+            //    Console.WriteLine("Toplam: " + z);
+            //}
+
+            //sum();
 
             #endregion
+
+            #region Geriye Değer Döndürmeyen String Parametreli Metotlar 
+
+            //void CustomerName(string name)
+            //{
+            //    Console.WriteLine("Müşteri Adı: " + name);
+            //}
+
+            //CustomerName("Nursena");
+
+
+            //void CustomerCard(string name, string surname)
+            //{
+            //    Console.WriteLine("Müşteri : " + name + " " +  surname);
+
+            //}
+
+            //CustomerCard("Nursena", "Kaya");
+            //CustomerCard("Büşra", "Ata");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar 
+
+            //void Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    Console.WriteLine("Toplam: " + result);
+            //}
+            //Sum(5, 10);
+
+                
+
+            #endregion
+
+
+            Console.Read();
         }
     }
 }
